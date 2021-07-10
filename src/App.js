bed68ad25e8a0420b7a4f930ba22574b7d67625c
Fsesnick <fernandasesnick@gmail.com>
2021-07-10 13:59:05 -0300

    API git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {useStage} from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App(props) {
-  
+  const [usuario, setUsuario] = useState('');
+  function handlePesquisa(){
+    //console.log(usuario);
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response =>console.log(response.data));
+  }
   return (
-    <div className="App">
-      <h1>{props.title} {props.user}</h1>
-      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuario"/>
-      <button type="button">Pesquisar</button>
-    </div>
+    <>
+      <input className="usuarioInput" placeholder="Usuario" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
